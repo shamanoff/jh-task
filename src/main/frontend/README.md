160571211afefccf469ed0d56b7b7c2f9e8deb61
Shamanoff <shamanoff@gmail.com>
2017-11-15 16:41:43 +0200

    work

--- src/main/frontend/README.md
+++ src/main/frontend/README.md
@@ -5,6 +5,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
+Run `npm start` for dev server with proxy `http://localhost:8080/`.
 
 ## Code scaffolding
 
--- src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../task.service";
+import {Task} from "../task.model";
 
 @Component({
   selector: 'app-tasks-list',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TasksListComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit() {
+    this.taskService.getTasks()
+      .subscribe(
+        (tasks: any[]) => {
+          this.tasks = tasks
+        },
+        (error) => console.log(error)
+      );
+
+    this.taskService.onTaskAdded.subscribe(
+      (task: Task) => this.tasks.push(task)
+    );
+  }
+
+  getDueDateLabel(task: Task){
+    return task.completed ? 'label-success' : 'label-primary';
+  }
+
+  onTaskChange(event, task) {
+    this.taskService.saveTask(task,event.target.checked).subscribe();
   }
 
 }
