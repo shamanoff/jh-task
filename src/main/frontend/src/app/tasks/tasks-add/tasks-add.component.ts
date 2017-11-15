2d79cd47cc856c5d1a7209fe785f447be9864a7d
Shamanoff <shamanoff@gmail.com>
2017-11-15 12:30:26 +0200

    work

---
+++ src/main/frontend/src/app/tasks/tasks-add/tasks-add.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tasks-add',
+  templateUrl: './tasks-add.component.html',
+  styleUrls: ['./tasks-add.component.scss']
+})
+export class TasksAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
